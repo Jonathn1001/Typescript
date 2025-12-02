d8c3f1ae287a2a4ba6d8efe0eeaca2ae660b3f45
elgnas <sangle1001.dev@gmail.com>
2025-12-02 20:12:03 +0700

    refactor: standardize string quotes in generics.ts

--- code/08-generics/generics.ts
+++ code/08-generics/generics.ts
@@ -1,11 +1,11 @@
-let names: Array<string> = ['Max', 'Anna'];
+let names: Array<string> = ["Max", "Anna"];
 
 type DataStore<T> = {
   [key: string]: T;
 };
 
 let store: DataStore<string | boolean> = {};
-store.name = 'Max';
+store.name = "Max";
 store.isInstructor = true;
 
 let nameStore: DataStore<string> = {};
@@ -14,18 +14,18 @@ function merge<T, U>(a: T, b: U) {
   return [a, b];
 }
 
-const ids = merge(1, 'Max');
+const ids = merge(1, "Max");
 
 function mergeObj<T extends object, U extends object>(a: T, b: U) {
   return { ...a, ...b };
 }
 
-const merged = mergeObj({ userName: 'Max' }, { age: 35 });
+const merged = mergeObj({ userName: "Max" }, { age: 35 });
 console.log(merged);
 
 class User<T> {
   constructor(public id: T) {}
 }
 
-const user = new User('i1');
-user.id
+const user = new User("i1");
+user.id;
