0c59068e96df7c5984b8f8f1aee8dc2a2ce76842
elgnas <sangle1001.dev@gmail.com>
2025-12-04 11:18:36 +0700

    refactor: standardize string quotes and enhance math functions in typeof.ts

--- code/10-deriving-types/typeof.ts
+++ code/10-deriving-types/typeof.ts
@@ -1,20 +1,36 @@
-let userName = 'Max';
+let userName = "Max";
 
 console.log(typeof userName);
 
 type UserName = typeof userName;
 
 const settings = {
-  difficulty: 'easy',
+  difficulty: "easy",
   minLevel: 10,
   didStart: false,
-  players: ['John', 'Jane']
+  players: ["John", "Jane"],
 };
 
 // type Settings = typeof settings;
 
 function loadData(s: typeof settings) {
   // ...
+  console.log("type", s);
 }
 
 loadData(settings);
+
+function sum(a: number, b: number) {
+  return a + b;
+}
+function subtract(a: number, b: number) {
+  return a - b;
+}
+
+type SumFn = typeof sum;
+type SubtractFn = typeof subtract;
+
+function performMathAction(cb: SumFn | SubtractFn) {
+  // some code...
+  return cb;
+}
